6111d0a14afb096f1c3b284b211235a184f37c5e
Cesar <32628077+xonex1208@users.noreply.github.com>
2019-10-15 07:24:05 -0500

    App casi al 100% v. 2.0

--- tienda/app/src/main/java/com/aortizg/zoo/ui/about/AboutViewModel.java
+++ tienda/app/src/main/java/com/aortizg/zoo/ui/about/AboutViewModel.java
@@ -10,7 +10,7 @@ public class AboutViewModel extends ViewModel {
 
     public AboutViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is tools fragment");
+        mText.setValue("Créditos");
     }
 
     public LiveData<String> getText() {
